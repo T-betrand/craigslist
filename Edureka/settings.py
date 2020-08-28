d925e2f5c65e0ff534dfa869bfc11ac1651dcc53
tebo <tebobertrand@gmail.com>
2020-08-28 12:34:53 +0100

    updated settings

--- Edureka/settings.py
+++ Edureka/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@lg459_vc#8hazu6m*3x*8zk8kgj)m!!62p43!x46$_4)6%pto'
+# SECRET_KEY = '@lg459_vc#8hazu6m*3x*8zk8kgj)m!!62p43!x46$_4)6%pto'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['polls-applica.herokuapp.com']
 
 
 # Application definition
